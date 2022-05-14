e143c91aaddc55b0bf65ae026eec44d2647edbc9
mogamoga <mogamoga.g1thu6@gmail.com>
2022-05-15 03:11:49 +0900

    cpu

--- js/logic/player/cpu.js
+++ js/logic/player/cpu.js
@@ -1,4 +1,17 @@
 
 class Cpu extends AbstractPlayer {
-    
+    selectExchangeCards() {
+        // 前提知識：this.cardsはカードの強さの昇順でソートされている。
+
+        // TODO 交換枚数
+
+        if (this.ranking <= this.allPlayerCount / 2) {
+            // TODO とりあえず単純に弱いカードを交換する。
+            return [this.cards[0], this.cards[1]]
+        }
+        else {
+            const cardsCount = this.cards.length;
+            return [this.cards[cardsCount - 2], this.cards[cardsCount - 1]];
+        }
+    }
 }
